e2bb24b882dd39552f69c043155a206b60950e08
aliyyulmanbinus <aliyyulmanbinus@gmail.com>
2025-11-14 10:52:06 +0700

    add about us page

--- src/app/template/class-online/class-online.component.ts
+++ src/app/template/class-online/class-online.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '@components/class-online/header/header.component';
 import { BannerComponent } from '@components/class-online/banner/banner.component';
 import { HeaderMenuComponent } from '@components/class-online/header-menu/header-menu.component';
+import { AboutUsComponent } from '@components/class-online/about-us/about-us.component';
 @Component({
   selector: 'app-class-online',
   standalone: true,
   imports: [
     HeaderComponent,
     BannerComponent,
-    HeaderMenuComponent
+    HeaderMenuComponent,
+    AboutUsComponent
   ],
   templateUrl: './class-online.component.html',
   styleUrl: './class-online.component.css'
